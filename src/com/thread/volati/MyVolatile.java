76454e9890eac92f4ff498d599068190cab72a36
ranwenjie <ranwenjie@didichuxing.com>
2021-04-22 20:33:29 +0800

    update volatile, countdownlatch

--- src/com/thread/volati/MyVolatile.java
+++ src/com/thread/volati/MyVolatile.java
@@ -1,7 +1,29 @@
 package com.thread.volati;
 
 //volatile 关键字
-public class MyVolatile {
+public class MyVolatile implements Runnable {
 
+    private volatile boolean flags = false;
 
+    @Override
+    public void run() {
+        while (!flags) {
+        }
+        System.out.println("thread exit.");
+    }
+
+    public void setFlags(boolean flags) {
+        this.flags = flags;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+
+        MyVolatile myVolatile = new MyVolatile();
+        Thread thread1 = new Thread(myVolatile);
+        thread1.start();
+
+        Thread.sleep(2*1000);
+
+        myVolatile.setFlags(true); //不加volatile时，并不会先thread1线程退出
+    }
 }
