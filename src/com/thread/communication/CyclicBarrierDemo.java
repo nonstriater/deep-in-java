846b802ae337c68a73a55a6db121c566d10c0ff1
ranwenjie <ranwenjie@didichuxing.com>
2021-04-25 15:01:08 +0800

    add cyclicBarrier

--- src/com/thread/communication/CyclicBarrierDemo.java
+++ src/com/thread/communication/CyclicBarrierDemo.java
@@ -1,5 +1,7 @@
 package com.thread.communication;
 
+import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
 import java.util.concurrent.CyclicBarrier;
 
 /**
@@ -7,5 +9,41 @@ import java.util.concurrent.CyclicBarrier;
  */
 public class CyclicBarrierDemo {
 
-    CyclicBarrier cb = new  CyclicBarrier(3);
+    static CyclicBarrier cyclicBarrier = new  CyclicBarrier(3);
+
+    static void print(String threadName) throws InterruptedException, BrokenBarrierException {
+        System.out.println(threadName + " work...");
+
+        Random random = new Random();
+        long prepareTime = random.nextInt(10000) + 100;
+        Thread.sleep(prepareTime);
+        System.out.printf(threadName + "work %d ms\n", prepareTime);
+
+        cyclicBarrier.await();
+        System.out.println("all prepare work done, all thread start work again");
+    }
+
+    public static void main(String[] args) throws BrokenBarrierException, InterruptedException {
+
+        for (int i = 0; i < 3; i++) {
+            final String threadName = "thread" + String.valueOf(i);
+            Thread t = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        print(threadName);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (BrokenBarrierException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            t.start();
+
+
+        }
+
+    }
+
 }
