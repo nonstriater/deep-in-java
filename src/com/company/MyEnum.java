38d8864763b5105c64c1993f1fadf55da318e951
ranwenjie <ranwenjie@didichuxing.com>
2021-06-08 16:05:57 +0800

    add enum

---
+++ src/com/company/MyEnum.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public enum MyEnum {
+
+    Agency1("a1"),//0
+    Agency2("a2");//1
+
+    private String name;
+
+    MyEnum(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public boolean contain(String name) {
+        return false;
+    }
+}
