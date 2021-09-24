7bdaeb269d654ad1ff564ea754f367946111e028
ranwenjie <ranwenjie@didichuxing.com>
2021-09-24 19:04:59 +0800

    left binsearch

--- src/main/java/com/nonstriater/deepinjava/algo/search/BinarySearch.java
+++ src/main/java/com/nonstriater/deepinjava/algo/search/BinarySearch.java
@@ -10,8 +10,11 @@ package com.nonstriater.deepinjava.algo.search;
 public class BinarySearch {
 
     public static void main(String[] args) {
-        int[] nums = {1,2,3,3,3,3,7};
-        System.out.println(binarySearchLowerBound(nums, 3, nums.length));
+        int[] nums = {1,2,2,3,3,3,3,7};
+        //System.out.println(binarySearchLowerBound(nums, 3, nums.length));
+
+        System.out.println(leftBinarySearch(nums, 3));
+
     }
 
     /**
@@ -19,7 +22,7 @@ public class BinarySearch {
      * @param A
      * @param target
      * @param n 数组长度
-     * @return
+     * @return  索引index
      */
     public static int binarySearchLowerBound(int[] A, int target, int n){
         int low = 0, high = n, mid;
@@ -35,6 +38,24 @@ public class BinarySearch {
             return low;
         else
             return -1;
+    }
+
+    public static int leftBinarySearch(int[] A, int target){
+        int low = 0, high = A.length, mid;
+
+        while(low < high){
+            mid = low + (high - low) / 2;
+
+            if(target < A[mid]){
+                high = mid ;
+            }else if (target == A[mid]) {
+                high = mid;
+            }else{
+                low = mid + 1;
+            }
+        }
+
+        return low;
     }
 
 }
---
+++ src/main/java/com/nonstriater/deepinjava/algo/test/Demo1.java
@@ -0,0 +1,13 @@
+package com.nonstriater.deepinjava.algo.test;
+
+public class Demo1 {
+
+    public static void main(String[] args){
+
+        solution();
+    }
+
+    public static void solution(){
+
+    }
+}
