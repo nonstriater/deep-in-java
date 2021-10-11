d4393823e08c16bc7b1c23399892c5139001be08
ranwenjie <ranwenjie@didichuxing.com>
2021-10-11 22:38:35 +0800

    update -m

--- src/main/java/com/nonstriater/deepinjava/algo/string/search/LongestPalindrome.java
+++ src/main/java/com/nonstriater/deepinjava/algo/string/search/LongestPalindrome.java
@@ -12,7 +12,63 @@ package com.nonstriater.deepinjava.algo.string.search;
  * 输出："bb"
  *
  * 思路1: 暴力
- * 思路2: 动态规划
+ * 思路2: 动态规划 ,  s[i,j] 是子串，
+ * 如果 S[i+1,j-1]S[i+1,j−1] 是回文串，那么只要 S[ i ] == S[ j ] ，就可以确定 S[i,j]S[i,j]也是回文串了。
+ *
  */
 public class LongestPalindrome {
+
+    public static void main(String[] args) {
+
+        System.out.println(longestPalindrome("babad"));
+
+        System.out.println(longestPalindrome("cbbd"));
+    }
+
+    /**
+     * 代码难点：
+     * 二维数组  boolean[][] dp = new boolean[n][n];
+     * @param s
+     * @return
+     */
+    public static String longestPalindrome(String s) {
+
+        //bad case
+        int n = s.length();
+        if(n == 0 || n==1){
+            return s;
+        }
+
+        //定义二位数组存储值，记录索引 i 和索引 j之间的元素是不是回文的
+        ////初始化全部都是false
+        boolean[][] dp = new boolean[n][n];
+
+        int start = 0; //回文串的开始位置
+        int max = 1; //回文串的最大长度
+        for(int i=0; i<n;i++) {
+            dp[i][i]=true;  //1个元素的肯定是true，这个回文没有什么太多意义，适合三个元素回文
+
+            //下面两个紧挨的元素相同的也变为true,之后就可以从三个开始找，这个情况适合四个元素的回文
+            if(i<n-1 && s.charAt(i) == s.charAt(i+1)){//n-1是为了防止s.charAt(i+1)发生越界异常
+                dp[i][i+1] = true;
+                start = i; //如果最后没有大于三的，那么最长就是2个元素的
+                max=2;
+            }
+        }
+
+        for (int l=3;l<=n;l++){ //l表示检索的子串长度，等于3表示先检索长度为3的子串
+            for(int i =0; i+l-1 < n; i++){
+                int j = l+i-1; //终止字符位置
+                if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1]==true){
+                    dp[i][j] = true;
+                    start = i;
+                    max = l;
+                }
+            }
+        }
+
+        return s.substring(start,start + max);
+    }
+
+
 }
