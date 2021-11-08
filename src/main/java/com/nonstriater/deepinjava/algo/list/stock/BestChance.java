415958f9cfcbac69f833df668969c6b3823401bf
ranwenjie <ranwenjie@didichuxing.com>
2021-11-09 01:21:03 +0800

    stock problem

--- src/main/java/com/nonstriater/deepinjava/algo/list/stock/BestChance.java
+++ src/main/java/com/nonstriater/deepinjava/algo/list/stock/BestChance.java
@@ -23,9 +23,34 @@ public class BestChance {
     }
 
 
-    public static int maxProfitK1(int[] list) {
+    public static int maxProfitK1(int[] prices) {
 
-        return 0;
+        int n = prices.length;
+        // base case: dp[-1][0] = 0, dp[-1][1] = -infinity
+        int dp_i_0 = 0, dp_i_1 = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            // dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i])
+            dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+            // dp[i][1] = max(dp[i-1][1], -prices[i])
+            dp_i_1 = Math.max(dp_i_1, -prices[i]);
+        }
+        return dp_i_0;
+
     }
+
+    // 空间复杂度优化版本
+    int maxProfit_k_inf(int[] prices) {
+        int n = prices.length;
+        int dp_i_0 = 0, dp_i_1 = Integer.MIN_VALUE;
+        for (int i = 0; i < n; i++) {
+            int temp = dp_i_0;
+            dp_i_0 = Math.max(dp_i_0, dp_i_1 + prices[i]);
+            dp_i_1 = Math.max(dp_i_1, temp - prices[i]);
+        }
+        return dp_i_0;
+    }
+
+
+
 
 }
