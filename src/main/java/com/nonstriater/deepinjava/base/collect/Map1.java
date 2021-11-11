ea3f4e350037dbd8af19aca5fbee269f17faed85
ranwenjie <ranwenjie@didichuxing.com>
2021-11-11 21:50:45 +0800

    add map & set

--- src/main/java/com/nonstriater/deepinjava/base/collect/Map1.java
+++ src/main/java/com/nonstriater/deepinjava/base/collect/Map1.java
@@ -10,8 +10,18 @@ public class Map1 {
 
     public static void main(String[] args) {
 
-        Map<String, String> m = new HashMap<>();
-        m.put("k1", "v1");
+        {
+            Map<String, String> m = new HashMap<>();
+            m.put("k1", "v1");
+            m.put("k2", "v2");
+
+            System.out.println(m.get("invalid key"));
+            System.out.println(m.get("k2"));
+
+            System.out.println(m.containsKey("invalid key"));
+            System.out.println(m.containsKey("k2"));
+        }
+
 
         Map<String, String> ht = new Hashtable<>();
 
--- src/main/java/com/nonstriater/deepinjava/base/collect/MySet.java
+++ src/main/java/com/nonstriater/deepinjava/base/collect/MySet.java
@@ -8,10 +8,20 @@ import java.util.stream.Collectors;
 public class MySet {
 
     public static void main(String[] args) {
-        Set<String> s= new HashSet<>(Arrays.asList("1","2"));
-        Set<String> newS = s.stream().filter(x -> x.equals("3")).collect(Collectors.toSet());
-        System.out.println(s);
-        System.out.println(newS);
+
+        {
+            Set<String> s= new HashSet<>(Arrays.asList("1","2"));
+            System.out.println(s.contains("2"));
+        }
+
+        {
+            Set<String> s= new HashSet<>(Arrays.asList("1","2"));
+            Set<String> newS = s.stream().filter(x -> x.equals("3")).collect(Collectors.toSet());
+            System.out.println(s);
+            System.out.println(newS);
+        }
+
+
     }
 
 
