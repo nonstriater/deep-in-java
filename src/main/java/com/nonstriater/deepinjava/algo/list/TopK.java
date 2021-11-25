5cdd3d9b98b744cfad2f9b7763b4447c4fc26461
ranwenjie <ranwenjie@didichuxing.com>
2021-11-25 20:22:13 +0800

    add topk

--- src/main/java/com/nonstriater/deepinjava/algo/list/TopK.java
+++ src/main/java/com/nonstriater/deepinjava/algo/list/TopK.java
@@ -6,4 +6,29 @@ package com.nonstriater.deepinjava.algo.list;
  * 给出两个升序数组A、B和长度m、n，求第k个大的
  */
 public class TopK {
+
+
+    //冒泡实现
+    public int findK(int[] nums, int k){
+        //base case
+        if (nums== null || nums.length < k) {
+            return -1;
+        }
+
+        for(int i=1; i<=k; i++){
+            for(int j=0; j<nums.length-i; j++) {
+                int next = j+1;
+                if(nums[j]> nums[next]){
+                    int tmp = nums[j];
+                    nums[j] = nums[next];
+                    nums[next] = tmp;
+                }
+            }
+        }
+
+        return nums[nums.length-k];
+    }
+
+
+
 }
